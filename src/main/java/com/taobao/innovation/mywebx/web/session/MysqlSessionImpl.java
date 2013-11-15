2e227379d1db93e4cc1c0ef4c510028541dca2e3
wuxiang <wuxiang@taobao.com>
2013-11-15 10:17:39 +0800

    增加一个数据库的session实现

---
+++ src/main/java/com/taobao/innovation/mywebx/web/session/MysqlSessionImpl.java
@@ -0,0 +1,20 @@
+package com.taobao.innovation.mywebx.web.session;
+
+/**
+ * User: voff12
+ * Date: 13-11-15
+ * Time: ионГ10:08
+ */
+public class MysqlSessionImpl  implements SessionStore{
+
+
+    @Override
+    public void setAttribute(String key, Object value) {
+    }
+
+
+    @Override
+    public Object getAttribute(String key) {
+
+    }
+}
